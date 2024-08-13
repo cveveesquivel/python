8849e8b345c1a82153cf51dd27b7b1006b3c7cea
cveveesquivel <cveveesquivel@unomaha.edu>
2024-08-13 16:58:02 -0500

    docstring & typehinting

--- television.py
+++ television.py
@@ -1,17 +1,23 @@
 class Television:
-    MIN_VOLUME = 0
-    MAX_VOLUME = 2
-    MIN_CHANNEL = 0
-    MAX_CHANNEL= 3
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL: int = 3
 
-    def __init__(self, status = False, muted = False, volume=MIN_VOLUME, channel=MIN_CHANNEL):
+    def __init__(self, status: bool = False, muted: bool = False, volume=MIN_VOLUME, channel=MIN_CHANNEL):
+        """
 
+        :param status: Determines if TV is on (True) or off (False
+        :param muted: Determines if volume is muted (True) or not muted (False)
+        :param volume: Gives volume of TV. Is affected by muted status
+        :param channel: Gives channel of TV.
+        """
         self.__status = status
         self.__muted = muted
         self.__volume = volume
         self.__channel = channel
 
-    def power(self):
+    def power(self) -> bool:
         if self.__status == False:
             self.__status = True
             return self.__status
@@ -19,7 +25,7 @@ class Television:
             self.__status = False
             return self.__status
 
-    def mute(self):
+    def mute(self) -> bool:
         if self.__status == False:
             return self.__muted
         elif self.__muted == False:
@@ -27,7 +33,7 @@ class Television:
             return self.__muted
         return self.__muted
 
-    def channel_up(self):
+    def channel_up(self) -> int:
         if self.__status == False:
             return self.__channel
         self.__channel += 1
@@ -35,7 +41,7 @@ class Television:
             self.__channel = Television.MIN_CHANNEL
         return self.__channel
 
-    def channel_down(self):
+    def channel_down(self) -> int:
         if self.__status == False:
             return self.__channel
         self.__channel -= 1
@@ -43,7 +49,7 @@ class Television:
             self.__channel = Television.MAX_CHANNEL
         return self.__channel
 
-    def volume_up(self):
+    def volume_up(self) -> int:
         if self.__status == False:
             return self.__volume
         self.__volume += 1
@@ -52,7 +58,7 @@ class Television:
             self.__volume = Television.MAX_VOLUME
         return self.__volume
 
-    def volume_down(self):
+    def volume_down(self) -> int:
         if self.__status == False:
             return self.__volume
         self.__volume -= 1
@@ -61,7 +67,7 @@ class Television:
             self.__volume = Television.MIN_VOLUME
         return self.__volume
 
-    def __str__(self):
+    def __str__(self) -> str:
         stat = self.__status
         vol = self.__volume
         if self.__muted == True:
---
+++ test_television.py
@@ -0,0 +1,19 @@
+import pytest
+import television
+
+class test_television():
+
+    def test_init(self):
+        assert pytest.raises()  # add assertion here
+    def test_power(self):
+        pass
+    def test_mute(self):
+        pass
+    def test_channel_up(self):
+        pass
+    def test_channel_down(self):
+        pass
+    def test_volume_up(self):
+        pass
+    def test_volume_down(self):
+        pass
