84d5caa8cd69a4bbf9a4e1c2610528092abefbc7
cveveesquivel <cveveesquivel@unomaha.edu>
2024-08-16 18:27:29 -0500

    Update test_television.py

--- test_television.py
+++ test_television.py
@@ -1,19 +1,51 @@
 import pytest
-import television
+from television import *
 
-class test_television():
+
+class Test():
+    def setup_method(self):
+        self.tv1 = Television()
+
+    def teardown_method(self):
+        del self.tv1
 
     def test_init(self):
-        assert pytest.raises()  # add assertion here
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'  # add assertion here
+
     def test_power(self):
-        pass
+        self.tv1.power()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
+        self.tv1.power()
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
     def test_mute(self):
-        pass
+        self.tv1.volume_up()
+        self.tv1.mute()
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
     def test_channel_up(self):
-        pass
+        self.tv1.channel_up()
+        assert self.tv1.__str__() == 'Power = False, Channel = 1, Volume = 0'
+        self.tv1.channel_up()
+        assert self.tv1.__str__() == 'Power = False, Channel = 2, Volume = 0'
+
     def test_channel_down(self):
-        pass
+        self.tv1.power()
+        self.tv1.channel_up()
+        self.tv1.channel_up()
+        self.tv1.channel_up()
+        assert self.tv1.__str__() == 'Power = True, Channel = 3, Volume = 0'
+        self.tv1.channel_down()
+        assert self.tv1.__str__() == 'Power = True, Channel = 2, Volume = 0'
+        self.tv1.channel_down()
+        assert self.tv1.__str__() == 'Power = True, Channel = 1, Volume = 0'
+        self.tv1.channel_down()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
+        self.tv1.channel_down()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
+
     def test_volume_up(self):
         pass
+
     def test_volume_down(self):
         pass
